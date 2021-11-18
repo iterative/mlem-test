30c36cd8daa8a80bef0ec705b79bb5285384fefe
mike0sv <mike0sv@gmail.com>
2021-11-18 21:31:26 +0300

    new links

--- simple/generate.sh
+++ simple/generate.sh
@@ -1,7 +1,7 @@
 set -exu
 mlem init
 python src/generate_data.py
-python src/train.py data/train data/model
-mlem link data/model latest
-mlem apply latest data/test_x -m predict_proba -o data/pred
-python src/score.py data/pred data/test_y scores.json
+python src/train.py train model
+mlem link model latest
+mlem apply latest test_x -m predict_proba -o pred
+python src/score.py pred test_y scores.json
